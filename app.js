de7c7414478a959014e54943d4744be89cf72a92
agbilotia1998 <agbilotia1998@gmail.com>
2017-03-24 04:57:19 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+NODE_MODULES_CACHE=false;
+var express=require("express"),
+    app=express(),
+    bodyParser=require("body-parser"),
+    mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+    sg = require('sendgrid')(process.env.SENDGRID_API_KEY),
+    passport=require("passport"),
+    LocalStrategy=require("passport-local"),
+    passportLocalMongoose=require("passport-local-mongoose");
+    cookieParser=require("cookie-parser");
