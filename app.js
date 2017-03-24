42a6cc5340c68727fca00b4b85f4b422a1da6db6
ankur <ankurj630@gmail.com>
2017-03-24 10:54:11 +0530

    Started Server and made logo

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ var express=require("express"),
     LocalStrategy=require("passport-local"),
     passportLocalMongoose=require("passport-local-mongoose");
     cookieParser=require("cookie-parser");
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('home.ejs')
+});
+
+app.listen(process.env.PORT || 5000, function() {
+    console.log('server started');
+});
