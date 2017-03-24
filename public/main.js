d62faa080d21910bdd9f0dfe9a50ae38196a6bc9
ankur <ankurj630@gmail.com>
2017-03-24 14:57:10 +0530

    Added hover-in and hover-out animation in images of home.html

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
 var self = this;
+var image;
 
 function changeImage(imageName) {
-    $('#' + 'imageName').attr('src', imageName + '1.jpg');
+    // $('#' + 'imageName').attr('src', imageName + '1.jpg');
+    document.getElementById(imageName).src = imageName + '1.jpg';
+    console.log('changed');
 }
 
 $('.hackimg').hover(function() {
-    var imageName = (this).attr('src');
+    var imageName = $(this).attr('id');
+    console.log(imageName);
+    self.image = imageName;
     changeImage(imageName);
-})
+    }, function() {
+        document.getElementById(image).src = image + '.jpg';
+});
 
 console.log('dome!!');
